4eaa0d7d217e5993e66763031754e0c2269931fd
ARMADOSIN <melvalera@mai.ru>
2023-03-03 04:31:59 +0300

    all

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopup = document.querySelector('.login-popup');
+let closeBtn = document.querySelector('.close-popup');
 
 loginButton.addEventListener('click', showPopup);
+closeBtn.addEventListener('click', closePopup);
 
 function showPopup(evt) {
     evt.preventDefault();
+
     loginPopup.classList.toggle("show-popup");
 };
+
+
+function closePopup(evt) {
+    loginPopup.classList.remove("show-popup");
+};
+
+loginPopup.addEventListener('click', (e) => {
+    if (e.target.classList.contains("show-popop")) {
+        closePopup();
+    }
+});
